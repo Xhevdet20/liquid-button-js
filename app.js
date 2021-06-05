38a41b22b5e3cdc07571bdcb7b48a1afbbdaf2b6
xhevdet20 <xhevdet1996@gail.com>
2021-06-05 16:00:55 +0200

    vertical liquidity added

---
+++ app.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('button');
+const turbulence = document.querySelector('feTurbulence');
+
+let verticalFrequency = 0.00001;
+
+turbulence.setAttribute('baseFrequency',verticalFrequency + "0.00001" );
+const steps = 30;
+const interval = 10;
+
+buttons.forEach(function(button){
+    button.addEventListener('mouseover', function(){
+        verticalFrequency = 0.00001;
+        
+        for (let i =0; i < steps; i++){
+            setTimeout(function(){
+                verticalFrequency +=0.009;
+                turbulence.setAttribute('baseFrequency',verticalFrequency + "0.00001" );
+            }, i*interval)
+        }
+        setTimeout(function(){
+            verticalFrequency = 0.00001;
+            turbulence.setAttribute('baseFrequency',verticalFrequency + "0.00001" );
+        }, steps * interval)
+    })
+})
